f52382814f96c0bd2685b2a946f031b9e76ff7b4
Shreyas Deshpande <shreyas.deshpande@trantorchd.com>
2026-03-13 11:49:57 +0530

    Initial .NET Framework project commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleDotNetFrameworkApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from CI pipeline practice!");
+        }
+    }
+}
